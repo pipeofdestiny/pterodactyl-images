1f10c3ae2e026ad77e62c9b6b7f07b9c8fa657d2
pipeofdestiny <pterodactyl@pipeofdestiny.com>
2022-08-30 01:37:03 +0200

    Add Sacred 2 Lobby

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,20 @@
 # ----------------------------------
-# Pterodactyl Core Dockerfile
+# Sacred 2 Lobby Emulator Dockerfile
 # Environment: Mono
-# Minimum Panel Version: 0.6.0
+# Minimum Panel Version: 1.3.1
 # ----------------------------------
 FROM        frolvlad/alpine-mono
 
-MAINTAINER  Pterodactyl Software, <support@pterodactyl.io>
+MAINTAINER  Pipe of Destiny Clan, <pterodactyl@pipeofdestiny.com>
 
 RUN         apk update \
-            && apk add --no-cache openssl curl sqlite \
+            && apk add --no-cache openssl curl sqlite xmlstarlet \
             && adduser -D -h /home/container container
 
 USER        container
 ENV         HOME=/home/container USER=container
 WORKDIR     /home/container
 
+COPY        ./lobby.config.template /lobby.config.template
 COPY        ./entrypoint.sh /entrypoint.sh
 CMD         ["/bin/ash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,38 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Make internal Docker gateway IP address available to processes.
+export INTERNAL_GW=`ip route get 1 | awk '{print $3;exit}'`
+
+# Make Lobby config path.
+export LOBBY_CONFIG="/home/container/lobby.config"
+
+# Remove previous configuration.
+if [ -e "${LOBBY_CONFIG}" ]; then
+    rm "${LOBBY_CONFIG}"
+fi
+
+# Create configuration from template.
+cp "/lobby.config.template" "${LOBBY_CONFIG}"
+
+edit_mysql_config() {
+    if [ $# -lt 2 ]; then
+        return 1
+    fi
+    local config="${3:-$LOBBY_CONFIG}"
+    xmlstarlet edit -L -u "/settings/database/mysql/add[@key='$1']/@value" -v "$2" "$config"
+    return $?
+}
+
+# Edit MySQL connection informations in configuration.
+edit_mysql_config "ip" "${MYSQL_HOST:-$INTERNAL_GW}" || exit 1
+edit_mysql_config "port" "${MYSQL_PORT:-3306}" || exit 1
+edit_mysql_config "name" "${MYSQL_DATABASE:-s2lobby}" || exit 1
+edit_mysql_config "user" "${MYSQL_USER:-s2lobby}" || exit 1
+edit_mysql_config "pass" "${MYSQL_PASSWORD:-}" || exit 1
+
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
